909629b03099c0636328cea4d934147edd5228c5
kamaevaju <107322383+kamaevaju@users.noreply.github.com>
2022-09-03 21:05:32 +0300

    Update ToDoList.js

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -6,8 +6,7 @@ export class BusinessList extends Component{
         BusinessList: [],
     }
     onChangeEvent(e){
-        this.setState({userInput: e});
-        
+        this.setState({userInput: e});   
     }
     addItem(input){
         if(input===''){
@@ -17,7 +16,7 @@ export class BusinessList extends Component{
         let listArray = this.state.BusinessList;
         listArray.push(input);
         this.setState({BusinessList : listArray, userInput: ''});
-    }
+        }
     }
     deleteItem(){
         let listArray = this.state.BusinessList;
@@ -35,24 +34,25 @@ export class BusinessList extends Component{
     render(){
         return(
             <div>
-                <form onSubmit={this.onFormSubmit}>
+              <form onSubmit={this.onFormSubmit}>
                 <div>
                     <input type="text"
                     placeholder="What are your plans?"
                     onChange={(e) => {this.onChangeEvent(e.target.value)} }
                     value={this.state.userInput} />
                 </div>
+
                 <div className="buttons">
                     <button className="btn add" onClick={()=>this.addItem(this.state.userInput)}>Add</button>
                     <button className="btn delete" onClick={()=> this.deleteItem()}>Delete</button>
                 </div>
+
                 <ul>
                     {this.state.BusinessList.map((item, index) =>(
                         <li onClick={this.crossedWord} key={index}>⚪ {item}</li>
                     ))}
                 </ul>
-                </form>
-
+              </form>
             </div>
         )
     }
